e39f502bd4ad863606ae1defd1883a9f75906c75
Ivan Koew <ivan@devnia.net>
2024-10-03 18:16:11 +0300

    try fetch google reviews

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,70 +6,49 @@ const Hero = () => {
 
     return (
         <div className='Hero'>
-            <div className='hero-wrapper'>
-                <div className='hero-image'>
-                    <video className='video' autoPlay loop muted>
-                        <source src="images/intro-video.mp4" type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
+            <video className='video' autoPlay loop muted>
+                <source src="images/intro-video.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
 
-                    <div className='h1-h2-wrapper'>
-                        <h1>ПОЧИСТВАНЕ НА DPF/FAP филтри  и катализатори на</h1>
-                        <h2>всякакъв вид моторни превозни средства (мпс)</h2>
-                    </div>
-
-                    <img src="/images/iso_9001_new.svg" alt="hero" className='iso' />
-
-                    <Link to={routes.contacts} className='is-green-box position-bottom-left'>
-                        Свържете се с нас
-                        <span className='additional-box'>+</span>
-                    </Link>
-                </div>
+            <div className='h1-h2-wrapper'>
+                <h1>ПОЧИСТВАНЕ НА DPF/FAP филтри  и катализатори на</h1>
+                <h2>всякакъв вид моторни превозни средства (мпс)</h2>
             </div>
 
-            <style jsx>{`
-            .Hero {
-                width: 100%;
-                height: fit-content;
-                height: 37.5rem;
-            }
-            .video {
-                width: 100%;
+            <img src="/images/iso_9001_new.svg" alt="hero" className='iso' />
 
-            }
-            .video source {
-                display: flex;
-                width: 100%;
-                height: 50%;
-            }
-            .hero-wrapper {
-                position: relative; 
-                height: fit-content;
-            }
-            .h1-h2-wrapper {
-                color: white;
-                position: absolute;
-                top: 50%;
-                left: var(--desktop-gap); 
-                transform: translateY(-50%);
-            }
-            .hero-image {
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 18.75rem;
-                background-size: cover;
-                /* background-image: url('/images/home.png'); */
-            }
-            .iso {
-                width: 7rem;
-                height: 7rem;
-                position: absolute;
-                bottom: var(--desktop-gap);
-                right: var(--desktop-gap);
-            }
-        `}</style>
+            <Link to={routes.contacts} className='is-green-box position-bottom-left'>
+                Свържете се с нас
+                <span className='additional-box'>+</span>
+            </Link>
+
+            <style jsx>{`
+                .Hero {
+                    width: 100%;
+                    height: fit-content;
+                    position: relative; 
+                }
+                .video {
+                    width: 100%;
+                    height: 37.5rem;
+                    object-fit: cover;
+                }
+                .h1-h2-wrapper {
+                    color: white;
+                    position: absolute;
+                    top: 50%;
+                    left: var(--desktop-gap); 
+                    transform: translateY(-50%);
+                }
+                .iso {
+                    width: 7rem;
+                    height: 7rem;
+                    position: absolute;
+                    bottom: var(--desktop-gap);
+                    right: var(--desktop-gap);
+                }
+            `}</style>
         </div>
     )
 }
--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,10 +1,117 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {getReviews} from '../ajax'
+import GoogleMapsReviews from './GoogleReviews'
 
 const Offer = () => {
+
+  // useEffect(() => {
+  //   const params = {
+  //     place_id: 'ChIJ0wjvRSeXpkARiqhljZ19hZI',          
+  //     fields: 'reviews',          
+  //     key: 'AIzaSyAb1wjFjUeyAEQN3GkADEGGTgWYeKaaXnE'              
+  //   };
+      
+  //   getReviews(params)
+  //     .then((res) => {
+  //         console.log(res);
+  //     })
+  //     .catch((err) => {
+  //         console.log(err);
+  //     })
+  // }, [])
+
+
   return (
-    <div>
-        Offer
-        <style>{``}</style>
+    <div className="Offer">
+        <div className='wrapper'>
+          <img src="images/dpf-reinigung.svg" alt="dpf-reinigung" className='dpf-reinigung'/>
+
+
+          <div className='text-wrapper'>
+            DPF CLEANING
+            <span className='additional-text'>
+              from 299BGN
+            </span>
+          </div>
+        </div>
+
+        <ul>
+          <li>
+            <img src="images/tick.svg" alt="tick" />
+            asdasdsd
+          </li>
+
+          <li>
+            <img src="images/tick.svg" alt="tick" />
+            asdasdsd
+          </li>
+
+          <li>
+            <img src="images/tick.svg" alt="tick" />
+            asdasdsd
+          </li>
+
+          <li>
+            <img src="images/tick.svg" alt="tick" />
+            asdasdsd
+          </li>
+
+          <li>
+            <img src="images/tick.svg" alt="tick" />
+            asdasdsd
+          </li>
+
+          <li>
+            <img src="images/tick.svg" alt="tick" />
+            asdasdsd
+          </li>
+        </ul>
+
+        <GoogleMapsReviews/>
+
+        <style jsx>{`
+          .Offer {
+            color: #333333;
+            position: absolute;
+            top: 20rem;
+            left: 5rem;
+            width: 20rem;
+            height: 25rem;
+            background-color: white;
+            padding: 2rem 1.5rem;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+          .wrapper {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 1rem;
+          }
+          .text-wrapper {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+          .dpf-reinigung {
+            width: 2.5rem;
+            height: 2.5rem;
+          }
+          .additional-text {
+            color: var(--primary-color);
+          }
+          ul {
+            list-style: none;
+            margin-top: 1rem;
+          }
+          li img {
+            width: 1rem;
+            height: 1rem;
+            margin-right: 0.5rem;
+          }
+        `}</style>
     </div>
   )
 }
